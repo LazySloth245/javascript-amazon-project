e507d91f132bfa9af22b8e1ca57ff81666330f83
LazySloth245 <hanhtr245@gmail.com>
2024-08-07 16:17:03 +0700

    move addToCart() into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,21 @@
 export const cart = [];
+
+export function addToCart(productId) {
+  const quantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+
+  let productExisted = false;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      productExisted = true;
+      cartItem.quantity += quantity;
+    }
+  });
+
+  if (!productExisted) {
+    cart.push({
+      productId,
+      quantity
+    });
+  }
+}
