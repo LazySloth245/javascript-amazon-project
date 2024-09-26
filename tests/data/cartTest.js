4110c6264272ca5807b21b218ef4897170e8fcd5
LazySloth <hanhtr245@gmail.com>
2024-09-26 10:52:53 +0700

    rewrite cart test with beforeEach() and afterEach()

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -1,9 +1,24 @@
 import {addToCart, cart, loadFromStorage} from "../../data/cart.js";
 
 describe('test suite: addToCart', () => {
-  it('adds an existing product to the cart', () => {
+  //create a mock Input element that's used in addToCart function:
+  let mockInputElement;
+
+  beforeEach(() => {
     spyOn(localStorage, 'setItem');
 
+    mockInputElement = document.createElement('input');
+    mockInputElement.className = 'js-quantity-selector-e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
+    mockInputElement.value = 1;
+    document.body.appendChild(mockInputElement);
+  });
+
+  afterEach(() => {
+    //remove the mock element:
+    document.body.removeChild(mockInputElement);
+  });
+
+  it('adds an existing product to the cart', () => {
     spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([{
         productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
@@ -13,35 +28,19 @@ describe('test suite: addToCart', () => {
     });
     loadFromStorage();
 
-    //create a mock Input element that's used in addToCart function:
-    const mockInputElement = document.createElement('input');
-    mockInputElement.className = 'js-quantity-selector-e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
-    mockInputElement.value = 1;
-    document.body.appendChild(mockInputElement);
-
     addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
 
     expect(cart.length).toEqual(1);
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
     expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart[0].quantity).toEqual(2);
-
-    //remove the mock element:
-    document.body.removeChild(mockInputElement);
   });
 
   it('adds a new product to the cart', () => {
-    spyOn(localStorage, 'setItem');
-    
     spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([]);
     });
     loadFromStorage();
-
-    const mockInputElement = document.createElement('input');
-    mockInputElement.className = 'js-quantity-selector-e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
-    mockInputElement.value = 1;
-    document.body.appendChild(mockInputElement);
 
     addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
 
@@ -49,7 +48,5 @@ describe('test suite: addToCart', () => {
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
     expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart[0].quantity).toEqual(1);
-
-    document.body.removeChild(mockInputElement);
   });
 });
