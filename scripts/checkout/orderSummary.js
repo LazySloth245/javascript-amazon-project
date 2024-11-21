8684e7bd9a5a3512112e64f54bb4c23d455e6806
LazySloth245 <hanhtr245@gmail.com>
2024-11-22 00:03:26 +0700

    done exercise 16j: add test case "update delivery option" in orderSummaryTest.js

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -87,12 +87,12 @@ export function renderOrderSummary() {
       const isChecked = (deliveryOption.id === deliveryOptionId);
 
       html += `
-        <div class="delivery-option js-delivery-option"
+        <div class="delivery-option js-delivery-option js-delivery-option-${productId}-${deliveryOption.id}"
           data-product-id="${productId}"
           data-delivery-option-id="${deliveryOption.id}">
           <input type="radio"
             ${isChecked ? 'checked' : ''}
-            class="delivery-option-input"
+            class="delivery-option-input js-delivery-option-input-${productId}-${deliveryOption.id}"
             name="delivery-option-${productId}">
           <div>
             <div class="delivery-option-date">
